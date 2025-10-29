2218a9dcc51759f2d81f47b22c17560da828fcea
Gabriel Presense <gabrielpresense@gmail.com>
2025-10-29 20:23:33 -0300

    feature(home): creating heroSection

---
+++ cn-web/src/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HeroSection: React.FC = () => {
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <h1>Transforme sua paixão em negócio.</h1>
+                <h2 className="subtitle">Junte-se ao ChefNow.</h2>
+                <p>Conecte-se a clientes exclusivos, gerencie seus eventos e brilhe com seus talentos culinários.</p>
+                <a href="#cadastrar-chef" className="btn-hero">Cadastrar-se como Chef</a>
+            </div>
+        </section>
+    );
+};
+
+export default HeroSection;
+
