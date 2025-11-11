3a964c8944c8776ef23aa2b453d2ab4914c33b30
Pedro Henrique <pedrohal2503@gmail.com>
2025-11-11 19:47:00 -0300

    feature(backend): removing X logo

--- cn-web/src/components/Footer.tsx
+++ cn-web/src/components/Footer.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import facebookIcon from '../assets/facebook.svg';
-import instagramIcon from '../assets/instagran.svg';
-import xIcon from '../assets/x.svg';
+import React from "react";
+import facebookIcon from "../assets/facebook.svg";
+import instagramIcon from "../assets/instagran.svg";
 
 const Footer: React.FC = () => {
-    return (
-        <footer className="footer">
-            <div className="footer-logo">ChefNow</div>
-            <div className="footer-copyright">© 2025 ChefNow. Todos os direitos reservados.</div>
-            <div className="footer-social">
-                <div className="social-icons">
-                    <a href="https://www.facebook.com/?locale=pt_BR" className="social-icon">
-                        <img src={facebookIcon} alt="Facebook" />
-                    </a>
-                    <a href="https://www.instagram.com" className="social-icon">
-                        <img src={instagramIcon} alt="Instagram" />
-                    </a>
-                    <a href="https://x.com" className="social-icon">
-                        <img src={xIcon} alt="X (Twitter)" />
-                    </a>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="footer-logo">ChefNow</div>
+      <div className="footer-copyright">
+        © 2025 ChefNow. Todos os direitos reservados.
+      </div>
+      <div className="footer-social">
+        <div className="social-icons">
+          <a
+            href="https://www.facebook.com/?locale=pt_BR"
+            className="social-icon"
+          >
+            <img src={facebookIcon} alt="Facebook" />
+          </a>
+          <a href="https://www.instagram.com" className="social-icon">
+            <img src={instagramIcon} alt="Instagram" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
